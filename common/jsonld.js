f3edaa5b2db307bf3aff765af604cf4d89c6a748
Gregg Kellogg <gregg@greggkellogg.net>
2018-09-17 13:16:56 -0700

    Update previousPublication stuff from FPWD.

--- common/jsonld.js
+++ common/jsonld.js
@@ -2,30 +2,6 @@ const jsonld = {
   // Add as the respecConfig localBiblio variable
   // Extend or override global respec references
   localBiblio: {
-    "JSON-LD11": {
-      title: "JSON-LD 1.1",
-      href: "https://w3c.github.io/json-ld-syntax/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '11 September 2018'
-    },
-    "JSON-LD11-API": {
-      title: "JSON-LD 1.1 Processing Algorithms and API",
-      href: "https://w3c.github.io/json-ld-api/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '11 September 2018'
-    },
-    "JSON-LD11-FRAMING": {
-      title: "JSON-LD 1.1 Framing",
-      href: "https://w3c.github.io/json-ld-framing/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '11 September 2018'
-    },
     // aliases to known references
     "IEEE-754-2008": {
       title: "IEEE 754-2008 Standard for Floating-Point Arithmetic",
