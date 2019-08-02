2aa2337cd653212d2f1eb62562762ab4aca2e055
Gregg Kellogg <gregg@greggkellogg.net>
2019-08-02 13:53:52 -0700

    Fix document cross-references.

--- common/jsonld.js
+++ common/jsonld.js
@@ -2,43 +2,12 @@ const jsonld = {
   // Add as the respecConfig localBiblio variable
   // Extend or override global respec references
   localBiblio: {
-    "JSON-LD11": {
-      title: "JSON-LD 1.1",
-      href: "https://w3c.github.io/json-ld-syntax/",
-      authors: ["Gregg Kellogg", "Pierre-Antoine Champin"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '10 May 2019'
-    },
-    "JSON-LD11-API": {
-      title: "JSON-LD 1.1 Processing Algorithms and API",
-      href: "https://w3c.github.io/json-ld-api/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '10 May 2019'
-    },
-    "JSON-LD11-FRAMING": {
-      title: "JSON-LD 1.1 Framing",
-      href: "https://w3c.github.io/json-ld-framing/",
-      authors: ["Gregg Kellogg"],
-      publisher: "W3C",
-      status: 'FPWD',
-      date: '10 May 2019'
-    },
     // aliases to known references
     "IEEE-754-2008": {
       title: "IEEE 754-2008 Standard for Floating-Point Arithmetic",
       href: "http://standards.ieee.org/findstds/standard/754-2008.html",
       publisher: "Institute of Electrical and Electronics Engineers",
       date: "2008"
-    },
-    "PROMISES": {
-      title: 'Promise Objects',
-      href: 'https://github.com/domenic/promises-unwrapping',
-      authors: ['Domenic Denicola'],
-      status: 'unofficial',
-      date: 'January 2014'
     },
     "JSON.API": {
       title: "JSON API",
